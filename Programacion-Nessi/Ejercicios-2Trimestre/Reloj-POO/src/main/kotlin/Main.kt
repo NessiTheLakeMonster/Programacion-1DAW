0aa90b4a1e637ff8e65c92abc15d79a0a4d30517
NessiTheLakeMonster <72609546+NessiTheLakeMonster@users.noreply.github.com>
2023-02-16 20:26:22 +0100

    reloj

---
+++ Programacion-Nessi/Ejercicios-2Trimestre/Reloj-POO/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+// Ejercicio 1 del Tema 4 --> Programacion orientada a objetos
+
+import kotlin.concurrent.thread
+
+fun main(args: Array<String>) {
+
+    var tiempo = Reloj(12, 30, 55)
+
+
+
+    if (tiempo.segundo > 59) {
+        tiempo.minuto++
+    }
+    if (tiempo.minuto > 59) {
+        tiempo.hora = tiempo.cambioFormato()
+    }
+
+    Thread.sleep(100) // Esto sirve para que el tiempo vaya pasando
+
+
+}
---
+++ Programacion-Nessi/Ejercicios-2Trimestre/Reloj-POO/src/main/kotlin/Reloj.kt
@@ -0,0 +1,30 @@
+class Reloj {
+    var hora: Int = 0
+    var minuto: Int = 0
+    var segundo: Int = 0
+    var pila: Int = 0
+
+
+    constructor(h: Int, m: Int, s: Int) {
+        this.hora = h
+        this.minuto = m
+        this.segundo = s
+    }
+
+    fun cambioFormato() : Int { // Para cambiar a formato 12 horas
+        if (hora > 12) {
+            hora -= 12
+        }
+        return hora
+    }
+
+    fun recargarPila() {
+        do {
+            pila += (0..99).random()
+        } while (pila < 100)
+    }
+
+    override fun toString(): String {
+        return "Reloj(hora=$hora, minuto=$minuto, segundo=$segundo)"
+    }
+}
