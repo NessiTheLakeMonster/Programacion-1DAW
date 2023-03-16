75e9c3c040d19feb537d7ac34ec71ca9dbebbb29
NessiTheLakeMonster <72609546+NessiTheLakeMonster@users.noreply.github.com>
2023-03-16 12:12:43 +0100

    cambios en las funciones de ficheros

--- Programacion-Clase/Ficheros/Ejemplo1-Ficheros/src/main/kotlin/Main.kt
+++ Programacion-Clase/Ficheros/Ejemplo1-Ficheros/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -6,54 +7,65 @@ import java.nio.file.StandardCopyOption
 
 
 fun main(args: Array<String>) {
-    //creación de un fichero
-    //tenemos que tener en cuenta que tengamos permisos en el sistema operativo para escribir
-    val ruta = Paths.get("newfile.txt")
-    // La siguiente instrucción seguramente producira un error de permisos de escritura
-    // val ruta = Paths.get("c:/newfile.txt")
-    Files.createFile(ruta)
-
-    //metodos de Path
-    var path = Paths.get("newfile.txt");
-    //Devuelve la ruta completa del fichero
-    println( path.toString())
-    //Devuelve el nombre del fichero
-    println( path.getFileName())
-
-    //comprobar que existe un fichero
-    val sFichero = "newfile.txt";
-
-    var f = File(sFichero);
-    if (f.exists()){
-        System.out.println("El fichero existe");
-    } else {
-        System.out.println("El fichero no existe");
+    //! Creación de un fichero
+    try {
+        // Tenemos que tener en cuenta que tengamos permisos en el sistema operativo para escribir
+        val ruta = Paths.get("newfile.txt")
+        // La siguiente instrucción seguramente producirá un error de permisos de escritura
+        // val ruta = Paths.get("d:/newfile.txt")
+        Files.createFile(ruta)
+    } catch (e: IOException) {
+        println("Este fichero esta repetido")
     }
 
-    //borrar un fichero
-   // var target = Paths.get("newfile.txt")
-   // Files.delete(target) //Si el fichero no existe se produce una excepción
-   // Files.deleteIfExists(target) //Si el fichero no existe no pasa nada
+    //! Métodos de Path
+    //val path = Paths.get("newfile.txt");
+    //Devuelve la ruta completa del fichero
+    //println(path.toString())
+    //Devuelve el nombre del fichero
+    //println(path.getFileName())
 
-    //copiar y mover
-    var source: Path = Paths.get("newfile.txt")
-    var target= Paths.get("./Ejemplo/targetFile.txt")
+    //! Comprobar que existe un fichero
+    val sFichero = "newfile.txt";
 
-    // Copiar archivo
-    Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING)
+    val f = File(sFichero);
+    if (f.exists()) {
+        println("El fichero existe");
+    } else {
+        println("El fichero no existe");
+    }
 
-    // Mover archivo
-    Files.move(source, target, StandardCopyOption.REPLACE_EXISTING)
+    //! Borrar un fichero
+    // var target = Paths.get("newfile.txt")
+    // Files.delete(target) //Si el fichero no existe se produce una excepción
+    // Files.deleteIfExists(target) //Si el fichero no existe no pasa nada (MEJOR OPCIÓN)
 
+    // * Los siguientes métodos se van a utilizar menos
+
+        //! Copiar y mover
+        val source: Path = Paths.get("newfile.txt")
+        val target = Paths.get("./Ejemplo/targetFile.txt")
+
+        //! Copiar archivo
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING)
+        // StandardCopyOption es una clase estática que no necesita instancia para poder llamarla
+
+        //! Mover archivo
+        Files.move(source, target, StandardCopyOption.REPLACE_EXISTING)
+
+
+    //? Diferencia entre la clase Path y la clase Paths
     /*
-    La clase Path es una clase que representa una ruta en el sistema de archivos.
+    La clase Path --> es una clase que representa una ruta en el sistema de archivos.
     Proporciona métodos para manipular y examinar la ruta,
     y es utilizado por las clases que operan en el sistema de archivos,
     como Files. En resumen, Path representa una ruta de archivo.
 
-     Paths es una clase de utilidad que proporciona métodos para crear instancias de la clase Path.
+     Paths --> es una clase de utilidad que proporciona métodos para crear instancias de la clase Path.
      Se utiliza para obtener una instancia de Path para una ruta específica en el sistema de archivos
      y proporciona formas convenientes de crear rutas relativas o absolutas.
      En resumen, Paths es una clase de utilidad para crear instancias de Path.
      */
+
+
 }
