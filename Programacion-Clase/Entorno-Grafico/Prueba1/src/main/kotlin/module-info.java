b24dff3da75afe3ea94f7db1547b5b03dd53b3d9
Inés Mª Barrera <72609546+NessiTheLakeMonster@users.noreply.github.com>
2023-05-26 09:18:33 +0200

    proyecto examen

--- Programacion-Clase/Entorno-Grafico/Prueba1/src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.prueba1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens com.example.prueba1 to javafx.fxml;
-    exports com.example.prueba1;
-}
--- Programacion-Clase/Entorno-Grafico/Prueba2/src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.prueba2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-                requires kotlin.stdlib;
-    
-                            
-    opens com.example.prueba2 to javafx.fxml;
-    exports com.example.prueba2;
-}
